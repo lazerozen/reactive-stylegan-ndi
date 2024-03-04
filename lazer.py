2550b003c7a55a860207821ceca11836dcf65a0e
lazer <laszlo@bertholini.de>
2024-03-04 09:57:42 +0100

    added submodule and fixed timing fockup

--- lazer.py
+++ lazer.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
+
+import sys
+sys.path.append("stylegan3")
 import os
 import re
-import sys
 from typing import List, Optional, Tuple, Union
 import time
 import click
@@ -17,8 +19,8 @@ import copy
 import numpy as np
 import torch
 from threading import Thread
-import dnnlib
-from torch_utils import misc
+#import dnnlib
+from stylegan3.torch_utils import misc
 from pythonosc.osc_server import AsyncIOOSCUDPServer
 from pythonosc.dispatcher import Dispatcher
 import asyncio
@@ -36,24 +38,24 @@ import NDIlib as ndi
 class lazer:
     latent = 0.1
     randomize = False
+    fps = 1
 
     def sendVideo (self,img,ndi_send,video_frame):
         video_frame.data = img
         #video_frame.FourCC = ndi.FOURCC_VIDEO_TYPE_BGRX
         video_frame.FourCC = ndi.FOURCC_VIDEO_TYPE_RGBX
 
-        #start = time.time()
-        #while time.time() - start < 60 * 5:
+        
 
         ndi.send_send_video_v2(ndi_send, video_frame)
 
-        #start_send = time.time()
+       
 
         #for idx in reversed(range(200)):
             #img.fill(255 if idx % 2 else 0)
             #   ndi.send_send_video_v2(ndi_send, video_frame)
 
-        #print('200 frames sent, at %1.2ffps' % (200.0 / (time.time() - start_send)))
+        
 
 
         return 0
@@ -89,46 +91,86 @@ class lazer:
             G = pickle.load(f)['G_ema'].cuda()  # torch.nn.Module
             z = torch.randn([1, G.z_dim]).cuda()    # latent codes
             
+            previousLatent = self.latent
+            mustRun = True
+            lastRun = time.time()
+            lastGen = lastRun
+
             while True:
-                self.move(1,40)
-                print('loop start',end='')
+                # ms elapsed since last run
+                elapsed = (time.time() - lastRun)*1000
+                
+                # run frequency goal in ms
+                runEvery = 1000 / self.fps
+                if (elapsed < runEvery):
+                    await asyncio.sleep(.001)
+                    continue
+
+                # copy early for pseudo thread safety, consider lock
+                localMustRun = mustRun
+                if self.latent != previousLatent:
+                    localMustRun = True
+                mustRun = False
+
+                #self.move(1,40)
+                #print('loop start',end='')
                 if self.randomize:
                     z = torch.randn([1, G.z_dim]).cuda()    # latent codes
                     self.randomize = False
+                    localMustRun = True
 
                 c = None                                # class labels (not used in this example)
-                img = G(z, c)                           # NCHW, float32, dynamic range [-1, +1], no truncation
-                img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+                
+                if localMustRun:
+                    startGen = time.time()
+                    self.move(1,1)
+                    print("Generating at "+str(1/(time.time()-lastGen))+" fps",end='\r')
+                    img = G(z, c)                           # NCHW, float32, dynamic range [-1, +1], no truncation
+                    img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
 
-                imgInColor = cv.cvtColor(img[0].cpu().numpy(), cv.COLOR_RGB2RGBA)
-                self.sendVideo(imgInColor,ndi_send, video_frame)
+                    #endGen = time.time()
+                    #print('200 frames sent, at %1.2ffps' % (200.0 / (time.time() - start_send)))
+                    #self.move(2,0)
+                    #print('Generation took %1.2f ms' % (time.time() - startGen)* 1000,end='')
+                    imgInColor = cv.cvtColor(img[0].cpu().numpy(), cv.COLOR_RGB2RGBA)
+                    self.sendVideo(imgInColor,ndi_send, video_frame)
 
-                for i in (1,100):
-                    z.H[i] = self.latent
-                self.move(1,40)
-                print('loop done',end='')
-                await asyncio.sleep(.02)
+                    for i in (1,100):
+                        z.H[i] = self.latent
+
+                    lastGen = time.time()
+
+                #self.move(1,40)
+                #print('loop done',end='')
+                await asyncio.sleep(.001)
+
+                previousLatent = self.latent
+                lastRun = time.time()
 
     def filter_handler_latent(address, *args):
         # We expect one float argument
         if not len(args) == 1 or type(args[0]) is not float:
             return
-        
         myLazer.latent = args[0]
-        print('latent set to '+str(myLazer.latent), end='\r')
-        #print(f"{address}: {args}")
+        #print('latent set to '+str(myLazer.latent), end='\r')
     
     def filter_handler_randomize(address, *args):
         print('randomize activated', end='\r')
         myLazer.randomize = True
-        #print(f"{address}: {args}")
+
+    def filter_handler_targetfps(address, *args):
+        if not len(args) == 1 or type(args[0]) is not int:
+            return
+        print('set fps to '+str(args[0]), end='\r')
+        myLazer.fps = args[0]
 
     dispatcher = Dispatcher()
     dispatcher.map("/latent", filter_handler_latent)
     dispatcher.map("/randomize", filter_handler_randomize)
+    dispatcher.map("/targetfps", filter_handler_targetfps)
 
     ip = "127.0.0.1"
-    port = 1337
+    port = 161
 
     async def init_main(self, timer):
         server = AsyncIOOSCUDPServer((self.ip, self.port), self.dispatcher, asyncio.get_event_loop())
