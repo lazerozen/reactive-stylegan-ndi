703e64c0900e36272fcb9fcfd9aff1e9129eaaf7
lazer <laszlo@bertholini.de>
2024-03-13 13:52:30 +0100

    added comments

--- lazer/lazer.py
+++ lazer/lazer.py
@@ -3,24 +3,15 @@ from __future__ import print_function
 import sys
 sys.path.append("../stylegan3")
 sys.path.append("../lazer")
-import os
-import re
 from typing import List, Optional, Tuple, Union
 import time
-import click
 import dnnlib
 import numpy as np
-import PIL.Image
 import torch
-import click
-import pickle
 import re
 import cv2 as cv
-import copy
 import numpy as np
 import torch
-from threading import Thread
-from stylegan3.torch_utils import misc
 from pythonosc.osc_server import AsyncIOOSCUDPServer
 from pythonosc.dispatcher import Dispatcher
 import asyncio
@@ -39,73 +30,101 @@ import NDIlib as ndi
 from lazer._filterhandlers import filterhandler
 
 class lazer(filterhandler):
-    #osc config
-    osc_ip = "127.0.0.1"
-    osc_port = 161
-    
-    #ndi config
-    ndi_name = 'stylegan3-lazer'
+    """
+    The `lazer` class represents an object that generates and sends video frames using NDI (Network Device Interface).
+    It utilizes the StyleGAN model to render images based on specified parameters and sends them as video frames via NDI.
 
-    #where we look into the model
-    latent = [random.randint(0,1611312),random.randint(0,1611312)] #x,y
+    Attributes:
+        osc_ip (str): The IP address for OSC (Open Sound Control) communication.
+        osc_port (int): The port number for OSC communication.
+        ndi_name (str): The name of the NDI source.
+        latent (list): The x and y values of the latent vector used for generating images.
+        mustTransform (bool): Flag indicating if a new frame needs to be rendered due to a transformation.
+        translate_x (int): The translation value along the x-axis for the input transformation.
+        translate_y (int): The translation value along the y-axis for the input transformation.
+        rotation (int): The rotation angle for the input transformation.
+        step_y (int): The step size for the latent walk.
+        fps (int): The desired frames per second for rendering and sending video frames.
+        mustRun (bool): Flag indicating if a new frame needs to be rendered and sent.
+        printFrequency (int): The frequency in milliseconds for printing status information.
+        renderArgs (dnnlib.EasyDict): The render arguments for the StyleGAN model.
+        timeServerStart (float): The start time of the server.
+        ndiFrequency (int): The number of NDI frames sent per second.
+        ndiFramesSent (int): The total number of NDI frames sent.
+        imagesGenerated (int): The total number of images generated.
+        skippedLoop (int): The number of loops where no rendering was necessary.
+        modelChanged (bool): Flag indicating if the StyleGAN model has changed and needs to be reloaded.
+        randFactor (list): The random latent addition to add a random value to existing latents.
+    """
+class lazer(filterhandler):
 
-    #decides if a new frame has to rendered
-    mustTransform = True
-    translate_x = 0
-    translate_y = 0
-    rotation = 0
+    def __init__(self):
+        super().__init__()
 
-    #step size for latent walk
-    step_y = 100
+        # Initialize attributes
+        self.osc_ip = "127.0.0.1"
+        self.osc_port = 161
+        self.ndi_name = 'stylegan3-lazer'
+        self.latent = [random.randint(0,1611312),random.randint(0,1611312)]
+        self.mustTransform = True
+        self.translate_x = 0
+        self.translate_y = 0
+        self.rotation = 0
+        self.step_y = 100
+        self.fps = 15
+        self.mustRun = True
+        self.printFrequency = 50
+        self.renderArgs = dnnlib.EasyDict(
+            pkl             = 'C:\\git\\stylegan3\\models\\wikiart-1024-stylegan3-t-17.2Mimg.pkl',
+            w0_seeds        = [],
+            trunc_psi       = 1.6,
+            trunc_cutoff    = 16,
+            random_seed     = 0,
+            noise_mode      = 'const',
+            force_fp32      = False,
+            layer_name      = None,
+            sel_channels    = 3,
+            base_channel    = 0,
+            img_normalize   = False,
+            input_transform = [[1,0,0],[0,1,0],[0,0,1]],
+            untransform     = False,
+            img_scale_db    = 0
+        )
+        self.timeServerStart = time.perf_counter()
+        self.ndiFrequency = 0
+        self.ndiFramesSent = 0
+        self.imagesGenerated = 0
+        self.skippedLoop = 0
+        self.modelChanged = True
+        self.randFactor = [0,0]
 
-    #default fps
-    fps = 15
-    #decides if a new frame has to rendered
-    mustRun = True
+    def sendVideo(self, img, ndi_send, video_frame):
+        """
+        Sends the video frame via NDI.
 
-    #print frequency in ms
-    printFrequency = 50
+        Args:
+            img (numpy.ndarray): The image data.
+            ndi_send (ndi.SendInstance): The NDI send instance.
+            video_frame (ndi.VideoFrameV2): The NDI video frame.
 
-    #all render args for all the beauty
-    renderArgs = dnnlib.EasyDict(
-        pkl             = 'C:\\git\\stylegan3\\models\\wikiart-1024-stylegan3-t-17.2Mimg.pkl',
-        w0_seeds        = [],
-        trunc_psi       = 1.6,
-        trunc_cutoff    = 16,
-        random_seed     = 0,
-        noise_mode      = 'const',
-        force_fp32      = False,
-        layer_name      = None,
-        sel_channels    = 3,
-        base_channel    = 0,
-        img_normalize   = False,
-        input_transform = [[1,0,0],[0,1,0],[0,0,1]],
-        untransform     = False,
-        img_scale_db    = 0
-        ) 
-    
-    timeServerStart = time.perf_counter()
-    #number of ndi frames sent per second
-    ndiFrequency = 0
-    #number of ndi frames sent
-    ndiFramesSent = 0
-    #number of images generate
-    imagesGenerated = 0
-    #number of loops where no rendering was neccessary
-    skippedLoop = 0
-    # must redo model load stuff
-    modelChanged = True
-    # random latent addition to add a random value to existing latents
-    randFactor = [0,0]
-
-    def sendVideo (self,img,ndi_send,video_frame):
+        Returns:
+            int: 0 if successful.
+        """
         video_frame.data = img
         video_frame.FourCC = ndi.FOURCC_VIDEO_TYPE_RGBX
         ndi.send_send_video_v2(ndi_send, video_frame) 
         return 0
 
-    #TODO!
     def _get_pinned_buf(self, ref):
+        """
+        Helper function to get a pinned buffer.
+
+        Args:
+            ref (torch.Tensor): The reference tensor.
+
+        Returns:
+            torch.Tensor: The pinned buffer.
+        """
         key = (tuple(ref.shape), ref.dtype)
         buf = self._pinned_bufs.get(key, None)
         if buf is None:
@@ -114,24 +133,32 @@ class lazer(filterhandler):
         return buf
 
     def printStatus(self):
+        """
+        Prints the current status.
+        """
         print("Generating at "+str(1/(time.time()-self.lastRun))+" fps")
         print("Images generated: "+str(self.imagesGenerated))
         print("NDI frames sent: "+str(self.ndiFramesSent))
-        print("skipped loops: "+str(self.skippedLoop))
-        print("latentX: "+str(self.latent[0]))
-        print("latentY: "+str(self.latent[1]),end='')
-        
+        print("Skipped loops: "+str(self.skippedLoop))
+        print("LatentX: "+str(self.latent[0]))
+        print("LatentY: "+str(self.latent[1]), end='')
         print("\033[A\033[A\033[A\033[A\033[A\033[A")
 
     def randomizeSeeds(self):
+        """
+        Randomizes the seeds for latent vector.
+        """
         self.randFactor[0] = random.randint(0,1611312)
         self.randFactor[1] = random.randint(0,1611312)
         self.randomize = False
         return
 
     def setSeedsFromLatent(self):
-        randomizedLatentX = self.latent[0]+self.randFactor[0]
-        randomizedLatentY = self.latent[1]+self.randFactor[1]
+        """
+        Sets the seeds for latent vector based on the current latent coordinates.
+        """
+        randomizedLatentX = self.latent[0] + self.randFactor[0]
+        randomizedLatentY = self.latent[1] + self.randFactor[1]
 
         self.renderArgs["w0_seeds"] = [] # [[seed, weight], ...]
         for ofs_x, ofs_y in [[0, 0], [1, 0], [0, 1], [1, 1]]:
@@ -141,8 +168,14 @@ class lazer(filterhandler):
             weight = (1 - abs(randomizedLatentX - seed_x)) * (1 - abs(randomizedLatentY - seed_y))
             if weight > 0:
                 self.renderArgs["w0_seeds"].append([seed, weight])
-    
+
     def initNdi(self):
+        """
+        Initializes the NDI communication.
+
+        Returns:
+            tuple: The NDI send instance and video frame.
+        """
         if not ndi.initialize():
             print("Could not initialize NDI.")
             exit()
@@ -163,6 +196,9 @@ class lazer(filterhandler):
         return ndi_send, video_frame
 
     async def loop(self):
+        """
+        The main loop that renders and sends frames.
+        """
         ndi_send, video_frame = self.initNdi()
 
         renderer = Renderer()
@@ -205,14 +241,13 @@ class lazer(filterhandler):
                 self.renderArgs["input_transform"] = transform
                 self.mustTransform = False
 
-            #if True:
             if localMustRun:
                 self.lastRun = time.time()
 
                 self.setSeedsFromLatent()
 
                 res = renderer.render(**self.renderArgs)
-                self.imagesGenerated+=1
+                self.imagesGenerated += 1
 
                 if firstRun:
                     #clear stylegan module output
@@ -241,6 +276,12 @@ class lazer(filterhandler):
             await asyncio.sleep(.001)
     
     def setupDispatcher(self):
+        """
+        Sets up the OSC dispatcher.
+
+        Returns:
+            osc.dispatcher.Dispatcher: The OSC dispatcher.
+        """
         dispatcher = Dispatcher()
         dispatcher.map("/latentX", self.filter_handler_latent_x)
         dispatcher.map("/latentY", self.filter_handler_latent_y)
@@ -256,6 +297,15 @@ class lazer(filterhandler):
         return dispatcher
 
     async def init_main(self, timer):
+        """
+        Initializes the main program.
+
+        Args:
+            timer (float): The timer value.
+
+        Returns:
+            None
+        """
         dispatcher = self.setupDispatcher()
         
         server = AsyncIOOSCUDPServer((self.osc_ip, self.osc_port), dispatcher, asyncio.get_event_loop())
